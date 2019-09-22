86ffb9a4297293381076ce5bc2a2c749533503b5
Andrew Hum <huma3@mcmaster.ca>
2019-09-22 15:52:04 -0400

    Removed old code, creating object

---
+++ MovieBarcode/OldCode/genFramesOld.py
@@ -0,0 +1,103 @@
+#########################################################################################################
+
+""" def genFrames(movie):
+
+  #os.makedirs('Frames')
+
+  vidcap = cv2.VideoCapture(movie)
+  fps = vidcap.get(cv2.CAP_PROP_FPS)
+  frame_count = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+  duration = int(frame_count / fps)
+  
+  # print ("genFrames fps: ", fps)
+  # print ("genFrames frame_count: ", frame_count)
+  # print ("genFrames duration: ", duration)
+
+  vidcap.release()
+
+  interval = duration / 1.28 # 1280 frames / 1000 
+
+  vidcap = cv2.VideoCapture(movie)
+  success,image = vidcap.read()
+  count = 0
+  success = True
+
+  while success:
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,(count * interval)) # captures frame every interval
+    cv2.imwrite('Frames/frame%d.jpg' % count, image)     # save frame as JPEG file
+    success,image = vidcap.read()
+    count += 1
+    # sys.stdout.write("=")
+    # sys.stdout.flush()
+    # if (count % 3 == 0): sys.stdout.write("Generating Frames ...")
+    # elif (count % 2 == 0): sys.stdout.write("Generating Frames ..")
+    # sys.stdout.write("Generating Frames")
+    # os.system('cls' if os.name == 'nt' else 'clear')
+
+  sys.stdout.write("]\n")
+  print "Successfully generated ", count, "frames. "
+  return count 
+
+def main():
+  genFrames('Files/SpiderVerse.mkv')
+
+main()  """
+
+#########################################################################################################
+
+""" def calcInterval(movie):
+  vidcap = cv2.VideoCapture(movie)
+  fps = vidcap.get(cv2.CAP_PROP_FPS)
+  frame_count = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
+  duration = frame_count / fps
+
+  # print "genFrames fps: ", fps
+  # print "genFrames frame_count: ", frame_count
+  # print "genFrames duration: ", duration
+
+  vidcap.release()
+
+  interval = duration / 1.9 # Interval to generate 1280 Frames
+
+  return interval
+
+def genFrame(vidcap, interval, count):
+  success,image = vidcap.read()
+  success = True
+
+  vidcap.set(cv2.CAP_PROP_POS_MSEC,(count * interval)) # captures frame every interval
+  cv2.imwrite('Frames/frame%d.jpg' % count, image)     # save frame as JPEG file
+  success,image = vidcap.read()
+
+  return success """
+
+
+#########################################################################################################
+
+""" def main():
+  vidcap = cv2.VideoCapture("Files/testVid.mp4")
+  fps = vidcap.get(cv2.CAP_PROP_FPS)
+  print fps
+  frame_count = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
+  print frame_count
+  duration = frame_count / fps
+  
+  vidcap.set(cv2.CAP_PROP_POS_MSEC,(21540))
+  # image = vidcap.read()[1]
+
+  # print image[0][0][2] # R value
+  # print image[0][0][1] # G value
+  # print image[0][0][0] # B value
+
+  print genFrame(vidcap)
+
+main() """
+
+
+""" def main():
+  movie = "Files/testVid.mp4"
+  vidcap = cv2.VideoCapture(movie)
+  interval = calcInterval(vidcap)
+  print genFrame(vidcap, interval, 0)
+
+main() """
---
+++ MovieBarcode/OldCode/mainOld.py
@@ -0,0 +1,76 @@
+#########################################################################################################
+
+""" def main():
+    numFrames = genFrames.genFrames('Files/SpiderVerse.mkv')
+
+    print ("Calculating Average Colours...")
+    colourArray = avgColour.allColours(1280)
+    drawCode.barcode(1280, colourArray)
+    print ("Barcode Generated :)")
+    print ("Cleaning Files...")
+
+    filelist = [ f for f in os.listdir('Frames')]
+    for f in filelist:
+      os.remove(os.path.join('Frames', f))
+
+    print ("Program Complete.")
+
+    #print allAvgCols
+ """
+
+#########################################################################################################
+
+""" def main():
+  movie = "Files/SpiderVerse.mkv"
+  count = 0
+
+  title = movie.split("/")[1].split(".")[0]
+  print title
+
+  #genFrames
+  vidcap = cv2.VideoCapture(movie)
+  interval = genFrames.calcInterval(movie)
+
+  drawCode.drawCanvas(1280, title)
+  
+  while genFrames.genFrame(vidcap, interval, count):
+      drawCode.drawFrame(avgColour.avgColour('Frames/frame%d.jpg' % count), count, title)
+      print count
+      count += 1
+      #if count == 20: break
+      
+  
+  print "Number of Frames: ", count
+
+  filelist = [ f for f in os.listdir('Frames')]
+  for f in filelist:
+    os.remove(os.path.join('Frames', f))
+ """
+#########################################################################################################
+
+""" def main():
+  movie = "Files/SpiderVerse.mkv"
+  count = 0
+
+  title = movie.split("/")[1].split(".")[0]
+  print title
+
+  #genFrames
+  vidcap = cv2.VideoCapture(movie)
+  interval = genFrames.calcInterval(movie)
+  
+  while genFrames.genFrame(vidcap, interval, count):
+      if count == 0: drawCode.drawCanvas(title)
+      drawCode.drawFrame(avgColour.avgRowCol('Frames/frame%d.jpg' % count), count, title)
+      print count
+      count += 1
+      #if count == 30: break
+      
+  
+  print "Number of Frames: ", count
+
+  filelist = [ f for f in os.listdir('Frames')]
+  for f in filelist:
+    os.remove(os.path.join('Frames', f))
+ """
+#########################################################################################################
